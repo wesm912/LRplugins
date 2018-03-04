c48977dea20e945816046bffdbdb1c5698677617
wesm <wes.mitchell@ericsson.com>
2018-03-03 21:52:54 -0800

    General cleanup, Add Photos to Project
    Additional functionality to add selected photos to a project.
    Deposits the photos in the XX candidates collection, sets the state
    to fixMetadata.
    
    TO DO: set the title, caption, genre, location states

---
+++ newproject.lrdevplugin/AddToProject.lua
@@ -0,0 +1,129 @@
+--[[
+    AddToProject.lua
+--]]
+
+require "PluginInit"
+
+
+local LrFunctionContext = import 'LrFunctionContext'
+local LrBinding = import 'LrBinding'
+local LrDialogs = import 'LrDialogs'
+local LrView = import 'LrView'
+local LrApplication = import 'LrApplication'
+local LrTasks = import 'LrTasks'
+local catalog = LrApplication.activeCatalog()
+local LrPathUtils = import 'LrPathUtils'
+local LrFileUtils = import 'LrFileUtils'
+local LrLogger = import 'LrLogger'
+local myLogger = LrLogger( 'newProjectWorkflow' )
+local projectRoot = PluginInit.projectRoot
+local bind = LrView.bind
+
+local function getProjectsCollectionSet()
+    local projects = PluginInit.getCollection("Projects")
+    if projects == nil then
+        local collectionSets = catalog:getChildCollectionSets()
+        for _, cset in ipairs(collectionSets) do
+            if cset:getName() == "Projects" then
+                projects = cset
+                break
+            end
+        end
+    end
+    return projects
+end
+
+local function getProjects()
+    local result = {}
+    projects = getProjectsCollectionSet()
+    if projects then
+        result = projects:getChildCollectionSets()
+    end
+    return result
+end
+
+local function showProjectPickerDialog()
+    -- -- outputToLog("Entering showProjectPickerDialog")
+    LrTasks.startAsyncTask(
+        function()
+            local currentProject = PluginInit.selectedProject()
+            local projectNames = {}
+            local allProjects = getProjects()
+            -- outputToLog("Current project: " .. (currentProject or "NONE"))
+            local popupItems = {}
+            local chosenIndex = -1
+            for i, p in ipairs(allProjects) do
+                table.insert(popupItems, {title = p:getName(), value = i})
+                -- outputToLog("inserted " .. p:getName() .. " and value " .. i)
+                if p:getName() == currentProject then
+                    -- outputToLog("setting chosenIndex to " .. i)
+                    chosenIndex = i
+                end
+            end
+            LrFunctionContext.callWithContext( "showProjectPickerDialog", function( context )
+
+                local props = LrBinding.makePropertyTable( context )
+                props.chosenProject = currentProject or ""
+                props.chosenIndex = chosenIndex
+
+                local f = LrView.osFactory()
+
+                -- create view hierarchy
+                local contents = f:column {
+                    fill_horizontal = 1,
+                    spacing = f:control_spacing(),
+                    bind_to_object = props,
+                    -- default bound table is the one we made
+                    f:static_text {
+                        alignment = "left",
+                        title = "Choose a Project: "
+                    },
+
+                    f:group_box {
+                        fill_horizontal = 1,
+                        spacing = f:control_spacing(),
+                        f:popup_menu {
+                            value = bind 'chosenIndex',
+                            items = popupItems,
+                        },
+                    },
+                }
+
+                local result = LrDialogs.presentModalDialog {
+                        title = "Add Photos to Project",
+                        contents = contents,
+                }
+                -- outputToLog("Chose Project " .. props.chosenIndex)
+                -- outputToLog(result)
+
+                if ( result == 'ok') then
+                    local chosenProject = allProjects[props.chosenIndex]
+                    local candidates = nil
+                    for j, c in ipairs(chosenProject:getChildCollections()) do
+                        if c:getName() == chosenProject:getName() .. " candidates" then
+                            -- outputToLog("Found candidates " .. c:getName())
+                            candidates = c
+                            break
+                        end
+                    end
+                    if candidates then
+                        photos = catalog:getTargetPhotos()
+                        catalog:withWriteAccessDo("AddPhotosToCollection",
+                            function(context)
+                                candidates:addPhotos(photos)
+                                for _, photo in ipairs(photos) do
+                                    -- outputToLog("Added candidate " .. photo:getFormattedMetadata("fileName"))
+                                    photo:setPropertyForPlugin(_PLUGIN, 'workflowState', 'fixMetadata')
+                                end
+                            end
+                        )
+                    end
+                end
+            end) -- end main function
+        end) -- startAsyncTask
+end
+
+-- Now display the dialogs.
+
+showProjectPickerDialog()
+
