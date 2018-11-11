1662936c825232f0b86f22daef53c466260e217f
wesm <wes.mitchell@ericsson.com>
2018-11-11 01:15:40 -0800

    Dialog now writes changes to database

--- newproject.lrdevplugin/Filmstrip.lua
+++ newproject.lrdevplugin/Filmstrip.lua
@@ -48,6 +48,20 @@ function print_r (t, indent, done)
   return result
 end
 
+function getPhotoEditFields(photo)
+    local title = photo:getFormattedMetadata('title')
+    local caption = photo:getFormattedMetadata('caption')
+    local genre = photo:getFormattedMetadata('intellectualGenre')
+    return {title = title, caption = caption, genre = genre}
+end
+
+function savePhotoMetadataChanges(photo, args)
+    PluginInit.outputToLog("Setting title to " .. args['title'] .. "for photo " .. photo.localIdentifier)
+    photo:setRawMetadata('title', args['title'] or 'No Title')
+    photo:setRawMetadata('caption', args['caption'] or 'No Caption')
+    photo:setRawMetadata('intellectualGenre', args['genre'] or 'No Genre')
+end
+
 
 function Filmstrip.makeFilmStrip( args )
     PluginInit.outputToLog("Start makeFilmStrip")
@@ -61,85 +75,161 @@ function Filmstrip.makeFilmStrip( args )
     local content = {}
 
     PluginInit.outputToLog("Got " .. #args.photos .. " photos")
-    content = LrFunctionContext.callWithContext( 'makeFilmStrip', function( context )
-        local f = LrView.osFactory()
-        local properties = LrBinding.makePropertyTable( context )
-        properties.photos = args.photos
-        local photos = args.photos
-        properties.firstIndex = 1
-        properties.length = length
-        local cat_photos = {}
-        for i, photo in ipairs(args.photos) do
-            local pargs = {
-                height = pheight,
-                width = pheight,
-                frame_width = 1,
-                background_color = LrColor(128,128,128),
-                mouse_down = args.callback,
-                photo = photo,
-                visible = true,
-            }
-            table.insert(cat_photos, f:catalog_photo(pargs))
-        end
-        properties.selectedPhoto = photos[properties.firstIndex]
-        properties:addObserver("firstIndex", function(props, key, newValue)
-                PluginInit.outputToLog("Observer called with key " .. key ..
-                    " and newValue  " .. tostring(newValue))
-                if key == "firstIndex" then
-                    if newValue >= 1 and newValue <= length then
-                        properties.selectedPhoto = properties.photos[newValue]
-                        PluginInit.outputToLog("Observer: newValue is " .. tostring(newValue))
-                    end
-                end
-            end)
+    content = LrFunctionContext.callWithContext( 'makeFilmStrip',
+        function( context )
+            local f = LrView.osFactory()
+            local properties = LrBinding.makePropertyTable( context )
+            properties.photos = args.photos
+            local photos = args.photos
+            properties.firstIndex = 1
+            properties.length = length
+            properties.edit_metadata = {}
 
-        content = f:column {
-            bind_to_object = properties,
-            f:row {
-                f:catalog_photo({
-                    photo = LrView.bind('selectedPhoto')
-                    })
-            },
-            f:row {
-                f:push_button {
-                    title = "<",
-                    action = function (button)
-                        properties.firstIndex = math.max(properties.firstIndex - 1, 1)
-                        properties.selectedPhoto = properties.photos[properties.firstIndex]
-                    end,
-                    enabled = LrView.bind {
-                        key = "firstIndex",
-                        transform = function(value, tbl)
-                            return value > 1
-                        end
-                    }
+            for i, photo in ipairs(args.photos) do
+                properties.edit_metadata[photo.localIdentifier] = getPhotoEditFields(photo)
+            end
+            properties.selectedPhoto = photos[properties.firstIndex]
+            properties:addObserver("selectedTitle", function(props, key, newValue)
+                    PluginInit.outputToLog("Observer called with key " .. key ..
+                        " and newValue  " .. tostring(newValue))
+                end)
+            local function saveMetadataChanges(photo)
+                PluginInit.outputToLog("Saving metadata for photo " .. photo.localIdentifier)
+                LrTasks.startAsyncTask( function()
+                    catalog:withWriteAccessDo("saveMetadataChanges",
+                        function (context)
+                            PluginInit.outputToLog("Saving metadata for individual photo " .. photo.localIdentifier)
+                            savePhotoMetadataChanges(photo, properties.edit_metadata[photo.localIdentifier])
+                        end)
+                    end)
+            end
+            local function updateSelectedPhotoFields()
+                local id = properties.selectedPhoto.localIdentifier
+                properties.selectedTitle = properties.edit_metadata[id]['title']
+                properties.selectedCaption = properties.edit_metadata[id]['caption']
+                properties.selectedGenre = properties.edit_metadata[id]['genre']
+            end
+            updateSelectedPhotoFields()
+            content = f:column {
+                bind_to_object = properties,
+                f:row {
+                    f:catalog_photo({
+                        place_vertical = .05,
+    --                    place_horizontal = 5,
+                        photo = LrView.bind('selectedPhoto')
+                        }),
+                    f:group_box {
+    --                    place_vertical = 10,
+    --                    place_horizontal = 10,
+                        f:row {
+                            fill_horizonal = 1,
+                            spacing = f:label_spacing(),
+                            f:static_text {title = 'Title'},
+                            f:edit_field {
+                                value = LrView.bind('selectedTitle'),
+                                wraps = true,
+--                                immediate = true,
+                                validate = function(view, value)
+                                    PluginInit.outputToLog("Validating title field " .. value)
+                                    if #value == 0 then
+                                        return false, value, "Please type a non-empty string"
+                                    else
+                                        local id = properties.selectedPhoto.localIdentifier                                        properties.edit_metadata[id]['title'] = value
+                                        updateSelectedPhotoFields()
+                                        properties.edit_metadata[id]['title'] = value
+                                        PluginInit.outputToLog("New value of selectedTitle: " .. properties.selectedTitle)
+                                        return true, value, ""
+                                    end
+                                end
+                            },
+                        },
+                        f:row {
+                            fill_horizonal = 1,
+                            spacing = f:label_spacing(),
+                            f:static_text {title = 'Caption'},
+                            f:edit_field {
+                                value = LrView.bind('selectedCaption'),
+
+                                validate = function(view, value)
+                                    if #value == 0 then
+                                        return false, value, "Please type a non-empty string"
+                                    else
+                                        local id = properties.selectedPhoto.localIdentifier                                        properties.edit_metadata[id]['caption'] = value
+                                        properties.edit_metadata[id]['caption'] = value
+                                        updateSelectedPhotoFields()
+                                        return true, value, ""
+                                    end
+                                end,
+                                wraps = true},
+                        },
+                        f:row {
+                            fill_horizonal = 1,
+                            spacing = f:label_spacing(),
+                            f:static_text {title = 'Genre'},
+                            f:edit_field {
+                                value = LrView.bind('selectedGenre'),
+
+                                validate = function(view, value)
+                                    if #value == 0 then
+                                        return false, value, "Please type a non-empty string"
+                                    else
+                                        local id = properties.selectedPhoto.localIdentifier
+                                        properties.edit_metadata[id]['genre'] = value
+                                        updateSelectedPhotoFields()
+                                        return true, value, ""
+                                    end
+                                end,
+                                wraps = true},
+                        },
+                        f:push_button {
+                            title = "Save Changes",
+                            action = function()
+                                PluginInit.outputToLog("Save button action called")
+                                saveMetadataChanges(properties.selectedPhoto)
+                            end
+                        },
+                    },
                 },
-                f:push_button {
-                    title = ">",
-                    action = function (button)
-                        local old = properties.firstIndex
-                        PluginInit.outputToLog("Next: old firstIndex is " .. old)
-                        properties.firstIndex = math.min(properties.firstIndex + 1, length)
-                        PluginInit.outputToLog("Next: new firstIndex is " .. properties.firstIndex)
-                        properties.selectedPhoto = properties.photos[properties.firstIndex]
-                    end,
-                    enabled = LrView.bind {
-                        key = "firstIndex",
-                        transform = function(value, tbl)
-                            return value < length
-                        end
-                    }
+                f:row {
+                    f:push_button {
+                        title = "<",
+                        action = function (button)
+                            properties.firstIndex = math.max(properties.firstIndex - 1, 1)
+                            properties.selectedPhoto = properties.photos[properties.firstIndex]
+                            updateSelectedPhotoFields()
+                        end,
+                        enabled = LrView.bind {
+                            key = "firstIndex",
+                            transform = function(value, tbl)
+                                return value > 1
+                            end
+                        }
+                    },
+                    f:push_button {
+                        title = ">",
+                        action = function (button)
+                            local old = properties.firstIndex
+                            properties.firstIndex = math.min(properties.firstIndex + 1, length)
+                            properties.selectedPhoto = properties.photos[properties.firstIndex]
+                            updateSelectedPhotoFields()
+                        end,
+                        enabled = LrView.bind {
+                            key = "firstIndex",
+                            transform = function(value, tbl)
+                                return value < length
+                            end
+                        }
+                    },
                 },
-            },
-        }
-        return content
-    end)
+            }
+            return content
+        end)
     return content
 end
 
 -- Test
 function showFilmStripTest()
---    LrFunctionContext.callWithContext( "TestFilmStripDialog", function( context )
+    LrTasks.startAsyncTask( function()
         local photos = catalog:getTargetPhotos()
         local f = LrView.osFactory()
         local content = Filmstrip.makeFilmStrip({photos = photos})
@@ -148,7 +238,7 @@ function showFilmStripTest()
             title = "Filmstrip Test",
             contents = content,
         }
---    end)
+    end)
 end
 
 showFilmStripTest()
